f10aca5d5b5f8d8c1529d35c08db06806cf8d531
MoyillaLaliteswari <155725438+MoyillaLaliteswari@users.noreply.github.com>
2024-11-11 11:46:54 +0530

    Time: 2 ms (70.98%), Space: 27.8 MB (75.72%) - LeetHub

---
+++ 2716-prime-subtraction-operation/2716-prime-subtraction-operation.cpp
@@ -0,0 +1,43 @@
+class Solution {
+    void sieven(vector<int>&primes,int maxVal){
+        bool sieve[maxVal+1];
+        memset(sieve,false,sizeof(sieve));
+        for(int i=2;i*i<maxVal;i++){
+            if(sieve[i]==false){
+                for(int j=2;j*i<maxVal;j++){
+                    sieve[i*j]=true;
+                }
+            }
+        }
+        for(int i=2;i<maxVal;i++){
+            if(sieve[i]==false){
+                primes.push_back(i);
+            }
+        }
+    }
+public:
+    bool primeSubOperation(vector<int>& nums) {
+        vector<int>primes;
+        int maxi=*max_element(nums.begin(),nums.end());
+        sieven(primes,maxi);
+        
+        for(int i=0;i<nums.size();i++){
+            int diff = i == 0 ? nums[0] - 1 : nums[i] - nums[i-1] - 1;
+            int lb=lower_bound(primes.begin(),primes.end(),diff)-
+            primes.begin();
+            if(lb==primes.size() or primes[lb]>diff){
+                lb--;
+            }
+            if(lb<0){
+                if(i==0 or nums[i]>nums[i-1]){
+                    continue;
+                }
+                else{
+                    return false;
+                }
+            }  
+            nums[i]-=primes[lb];
+        }
+    return true;
+    }
+};
