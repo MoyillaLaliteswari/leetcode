7d92e081dbf50d34ca0f6a19edae5c5733cc45dd
MoyillaLaliteswari <155725438+MoyillaLaliteswari@users.noreply.github.com>
2025-01-31 17:44:27 +0530

    Time: 295 ms (36.39%), Space: 163.1 MB (30.72%) - LeetHub

---
+++ 0854-making-a-large-island/0854-making-a-large-island.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    int largestIsland(vector<vector<int>>& grid) {
+        if(grid.empty()) return 0;
+
+        int n=grid.size();
+        vector<vector<int>>labels(n,vector<int>(n,0));
+        unordered_map<int,int>size;
+        int label=1;
+        int maxSize=0;
+        int dirs[4][2]={{-1,0},{1,0},{0,-1},{0,1}};
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1 && labels[i][j]==0){
+                    int s=0;
+                    vector<pair<int,int>>st;
+                    st.push_back({i,j});
+                    labels[i][j]=label;
+                    while(!st.empty()){
+                        auto [x,y]=st.back();
+                        st.pop_back();
+                        s++;
+                        for(auto [dx,dy]:dirs){
+                            int nx=x+dx,ny=y+dy;
+                            if(nx>=0 && nx<n && ny>=0 && ny<n && grid[nx][ny]==1 && labels[nx][ny]==0){
+                                labels[nx][ny]=label;
+                                st.push_back({nx,ny});
+                            }
+                        }
+                    }
+                    size[label]=s;
+                    maxSize=max(maxSize,s);
+                    label++;
+                }
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 0) {
+                    unordered_set<int> neighborLabels;
+                    int total = 1;
+
+                    for (auto [dx, dy] : dirs) {
+                        int nx = i + dx, ny = j + dy;
+                        if (nx >= 0 && nx < n && ny >= 0 && ny < n &&
+                            grid[nx][ny] == 1) {
+                            neighborLabels.insert(labels[nx][ny]);
+                        }
+                    }
+
+                    for (int lbl : neighborLabels) {
+                        total += size[lbl];
+                    }
+
+                    maxSize = max(maxSize, total);
+                }
+            }
+        }
+
+        return maxSize;
+    }
+};
