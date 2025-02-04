b5998988c45db52c26160018bea7ada4aad509d3
MoyillaLaliteswari <155725438+MoyillaLaliteswari@users.noreply.github.com>
2025-02-04 19:23:34 +0530

    Time: 0 ms (100%), Space: 11.3 MB (2.63%) - LeetHub

---
+++ 1927-maximum-ascending-subarray-sum/1927-maximum-ascending-subarray-sum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxAscendingSum(vector<int>& nums) {
+        int sum=nums[0],m=nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(nums[i-1]<nums[i]){
+                sum+=nums[i];
+            }else{
+                sum=nums[i];
+            }
+            m=max(m,sum);
+        }
+        return m;
+    }
+};
